6a35b2a66c868feebbc7dc541384c573ddaed00e
野路菜々華 <coco_nana@M-Nanaka-Noji-MacBookPro.local>
2020-03-14 22:24:11 +0900

    変更した内容をメモ

---
+++ pic.js
@@ -0,0 +1,9 @@
+$(function() {
+   $('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+    slidesToShow:5,
+    slidesToScroll:1,
+});
+})
--- slidershow.js
+++ slidershow.js
@@ -1,10 +1,10 @@
-jQuery(function($) {
+(function($) {
     $('.bg-switcher').bgSwitcher({
         image: ['image/head_22.jpg','image/head_24.jpg','image/head_unico.jpg','image/head_unico2.jpg'], // 切り替え画像
         Interval: 5000, //切り替えの間隔 1000=1秒
         start: true, //$.fn.bgswitcher(config)をコールした時に切り替えを開始する
         loop: true, //切り替えをループする
-        shuffle:true;, //背景画像の順番をシャッフルする
+        shuffle:false, //背景画像の順番をシャッフルする
         effect: "fade", //エフェクトの種類 "fade" "blind" "clip" "slide" "drop" "hide"
         duration: 1000, //エフェクトの時間 1000=1秒
         easing: "swing", //エフェクトのイージング "swing" "linear"
